e80ecd89740d97384629efe5091bdd90ad540d2c
Nikita Matveev <91771676+matveevN@users.noreply.github.com>
2024-05-11 09:14:34 +0300

    Update app.go

--- golang/app.go
+++ golang/app.go
@@ -7,14 +7,32 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, 世界")
+	
+	fmt.Fprintln(w, "<h1>Hello, 世界</h1>")
+
+	
+	fmt.Fprintln(w, `
+	<style>
+		.cat {
+			color: #3E721D; /* Цвет кота */
+			font-size: 24px; /* Размер шрифта */
+			font-family: Arial, sans-serif; /* Шрифт */
+		}
+	</style>
+	<pre class="cat">
+     /\_/\  
+    / o o \ 
+  (   "   ) 
+   \~(*)~/  
+    \~Y~/   
+     |/|    
+     |_|    
+	</pre>
+	`)
 }
 
 func main() {
 	http.HandleFunc("/", handler)
-	fmt.Println("Running demo app. Press Ctrl+C to exit....")
+	fmt.Println("Running demo app. Press Ctrl+C to exit...")
 	log.Fatal(http.ListenAndServe(":8888", nil))
 }
-
-
-
